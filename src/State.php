3eed5be24b192f92a2ed7460edc073d75f6ceed1
amondar <zaxodu11@gmail.com>
2026-04-29 03:49:57 +0300

    Add eastern and western state grouping methods to `State` enum

--- src/State.php
+++ src/State.php
@@ -6,6 +6,7 @@ namespace Amondar\PhpStateFlow;
 
 enum State: string
 {
+    case AK = 'Alaska';
     case AL = 'Alabama';
     case AZ = 'Arizona';
     case AR = 'Arkansas';
@@ -14,6 +15,7 @@ enum State: string
     case CT = 'Connecticut';
     case DE = 'Delaware';
     case FL = 'Florida';
+    case HI = 'Hawaii';
     case GA = 'Georgia';
     case ID = 'Idaho';
     case IL = 'Illinois';
@@ -54,4 +56,118 @@ enum State: string
     case WV = 'West Virginia';
     case WI = 'Wisconsin';
     case WY = 'Wyoming';
+
+    public static function easternStateAbbreviations(): array
+    {
+        return [
+            self::ME->name,
+            self::NH->name,
+            self::VT->name,
+            self::MA->name,
+            self::RI->name,
+            self::CT->name,
+            self::NY->name,
+            self::PA->name,
+            self::NJ->name,
+            self::DE->name,
+            self::MD->name,
+            self::VA->name,
+            self::WV->name,
+            self::NC->name,
+            self::SC->name,
+            self::GA->name,
+            self::FL->name,
+            self::OH->name,
+            self::MI->name,
+            self::IN->name,
+            self::IL->name,
+            self::WI->name,
+            self::KY->name,
+            self::TN->name,
+            self::AL->name,
+            self::MS->name,
+        ];
+    }
+
+    public static function easternStateNames(): array
+    {
+        return [
+            self::ME->value,
+            self::NH->value,
+            self::VT->value,
+            self::MA->value,
+            self::RI->value,
+            self::CT->value,
+            self::NY->value,
+            self::PA->value,
+            self::NJ->value,
+            self::DE->value,
+            self::MD->value,
+            self::VA->value,
+            self::WV->value,
+            self::NC->value,
+            self::SC->value,
+            self::GA->value,
+            self::FL->value,
+            self::OH->value,
+            self::MI->value,
+            self::IN->value,
+            self::IL->value,
+            self::WI->value,
+            self::KY->value,
+            self::TN->value,
+            self::AL->value,
+            self::MS->value,
+        ];
+    }
+
+    public static function westernStateAbbreviations(): array
+    {
+        return [
+            self::AK->name,
+            self::HI->name,
+            self::CA->name,
+            self::OR->name,
+            self::WA->name,
+            self::ID->name,
+            self::MT->name,
+            self::WY->name,
+            self::NV->name,
+            self::UT->name,
+            self::CO->name,
+            self::AZ->name,
+            self::NM->name,
+            self::TX->name,
+            self::OK->name,
+            self::KS->name,
+            self::NE->name,
+            self::SD->name,
+            self::ND->name,
+        ];
+    }
+
+    public static function westernStateNames(): array
+    {
+        return [
+            self::AK->value,
+            self::HI->value,
+            self::CA->value,
+            self::OR->value,
+            self::WA->value,
+            self::ID->value,
+            self::MT->value,
+            self::WY->value,
+            self::NV->value,
+            self::UT->value,
+            self::CO->value,
+            self::AZ->value,
+            self::NM->value,
+            self::TX->value,
+            self::OK->value,
+            self::KS->value,
+            self::NE->value,
+            self::SD->value,
+            self::ND->value,
+        ];
+    }
 }
